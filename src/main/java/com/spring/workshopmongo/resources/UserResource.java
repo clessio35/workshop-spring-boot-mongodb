7c01144959091b20dc773a6ac173a85342aa8fea
Clessio Silva <clessio.silva1989@gmail.com>
2025-02-27 09:47:20 -0300

    Get user by ID

--- src/main/java/com/spring/workshopmongo/resources/UserResource.java
+++ src/main/java/com/spring/workshopmongo/resources/UserResource.java
@@ -1,18 +1,20 @@
 package com.spring.workshopmongo.resources;
 
 import java.util.List;
-import java.util.stream.Collector;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.workshopmongo.domain.User;
 import com.spring.workshopmongo.dto.UserDTO;
 import com.spring.workshopmongo.services.UserService;
+import com.spring.workshopmongo.services.exception.ObjectNotFoundException;
 
 @RestController
 @RequestMapping(value="/users")
@@ -26,5 +28,12 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		Optional<User> obj = service.findById(id); // Assuming service returns Optional<User>
+		User user = obj.orElseThrow(() -> new ObjectNotFoundException("User not found with id: " + id));
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
